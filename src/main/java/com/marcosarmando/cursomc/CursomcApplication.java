f1b9c4ee0287ae1665016ab69ff74f459dbe29ae
Marcos Armando <marcos.ffilho@yahoo.com.br>
2019-07-26 15:43:21 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/marcosarmando/cursomc/CursomcApplication.java
+++ src/main/java/com/marcosarmando/cursomc/CursomcApplication.java
@@ -8,17 +8,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.marcosarmando.cursomc.domain.Categoria;
+import com.marcosarmando.cursomc.domain.Produto;
 import com.marcosarmando.cursomc.repositories.CategoriaRepository;
+import com.marcosarmando.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+		
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
-
 		
 	}
 
@@ -28,7 +31,19 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		 
+		cat1.getProduto().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProduto().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.save(Arrays.asList(cat1, cat2));
+		produtoRepository.save(Arrays.asList(p1,p2, p3));
 	}
 	
 }	
--- src/main/java/com/marcosarmando/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/marcosarmando/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,11 @@
 package com.marcosarmando.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.marcosarmando.cursomc.domain.Categoria;
 import com.marcosarmando.cursomc.services.CategoriaService;
 
